a461a736cd8420e37a210132bcc55eab4bb01564
Leonard Choo <leochoodev@gmail.com>
2021-08-23 14:22:04 +0900

    start fresh

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 // decorators add functionality to class
 @Module({
   imports: [],
-  controllers: [AppController], // like router in express
-  providers: [AppService],
+  controllers: [], // like router in express
+  providers: [],
 })
 export class AppModule {}
