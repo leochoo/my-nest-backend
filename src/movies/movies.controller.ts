0bdd11dd7327e776436dca09d32599e1507aaf47
Leonard Choo <leochoodev@gmail.com>
2021-08-23 15:18:18 +0900

    search added

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Post, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Body,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,23 +15,34 @@ export class MoviesController {
   getAll() {
     return 'getAll';
   }
-  @Get('/:id') // on NestJS, if you want sth, you must ask for it.
+
+  // the function order matters. we need to place this before search to make it believe that search isn't id. If the get(":id") is at the top, other get won't work.
+  @Get('search')
+  search(@Query('year') movieYear: number) {
+    return `searching for a movie made after ${movieYear}`;
+  }
+
+  @Get(':id') // on NestJS, if you want sth, you must ask for it.
   getOne(@Param('id') movieId: string) {
     return `Movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'create movie';
+  create(@Body() movieData) {
+    // console.log(movieData);
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `Deleted the movie with the id: ${movieId}`;
   }
 
-  @Patch('/:id') // updates some parts of the resource
-  patch(@Param('id') movieId: string) {
-    return `Patched the movie with the id: ${movieId}`;
+  @Patch(':id') // updates some parts of the resource
+  patch(@Param('id') movieId: string, @Body() updateMovieData) {
+    return {
+      updatedMovie: movieId,
+      ...updateMovieData,
+    };
   }
 }
