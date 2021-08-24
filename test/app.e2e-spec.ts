b9ddcace0c5d8f3f4c49b519649abbbfc948f650
Leonard Choo <leochoodev@gmail.com>
2021-08-24 17:09:28 +0900

    done

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -13,6 +13,13 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true, // converts data type on the fly
+      }),
+    );
     await app.init();
   });
 
@@ -28,7 +35,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('Post', () => {
+    it('Post 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -39,15 +46,41 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
-
+    it('Post 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'test movie',
+          year: 2020,
+          genres: ['test'],
+          director: 'Leonard Choo',
+          badkey: 'baddata',
+        })
+        .expect(400);
+    });
     it('Delete', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
   });
 
+  // during testing, the server might think that this :id given is string
+  // this is due to transform in our main.ts
+  // thus, we should remember to transform just like main.ts
   describe('/movies/:id', () => {
-    it.todo('GET');
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Title' })
+        .expect(200);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
